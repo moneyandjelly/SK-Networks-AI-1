78b836c6e9d451aaaa3e4810af5d30906360dad0
moneyandjelly <rmsdldy12@gmail.com>
2024-07-05 09:52:37 +0900

    feat: [이근] 개, 고양이 타겟팅하여 레이블 뽑기 [SKAIPx]

--- fastapiAI/GeunLee/fastapi_demo/convolution_neural_network/repository/cnn_repository.py
+++ fastapiAI/GeunLee/fastapi_demo/convolution_neural_network/repository/cnn_repository.py
@@ -5,3 +5,7 @@ class ConvolutionNeralNetworkRepository(ABC):
     @abstractmethod
     def loadCifar10Data(self):
         pass
+
+    @abstractmethod
+    def filteringClasses(self, imageList, labelList, targetClassList):
+        pass
